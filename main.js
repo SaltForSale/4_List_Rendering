a388a74af467fd527abc351a00fbea3ab38bc3c5
Salt_For_Sale <elee823@gmail.com>
2024-04-30 11:28:41 -0400

    done

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
-  data () {
+  data() {
     return {
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inStock: true,
-      details: ['50% cotton', '30% wool', '20% polyester']
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      sizes: ['small', 'medium', 'large'],
+      variants: [{
+        id: 1111, color: 'green'
+      },
+      { id: 1112, color: 'blue' }]
     }
   }
 })
